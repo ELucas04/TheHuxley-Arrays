381e18f0da37f528efa3025d8c0d154be89c56c3
Emanuel Lucas <101639155+ELucas04@users.noreply.github.com>
2022-07-05 23:30:42 -0300

    Add files via upload

---
+++ ChandlerVsPhoebe.java
@@ -0,0 +1,37 @@
+package questoes;
+
+import java.io.*;
+import java.util.*;
+
+public class ChandlerVsPhoebe {
+  public static void main(String args[]) {
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt();
+    int[][] mat = new int[n][n];
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            mat[i][j] = input.nextInt();
+        }
+        System.out.println();
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (mat[j][i] < 0) {
+                System.out.printf("%d", mat[j][i] * 2);
+            }
+            else {
+                System.out.printf("%d", mat[j][i]);
+            }
+            if (j < n-1) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+    
+    
+    input.close();
+  }
+}
